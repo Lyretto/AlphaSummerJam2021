45793e0521f4154ea61e1f53fd097f917e220a44
Richard <yegac000@gmail.com>
2021-09-04 19:23:21 +0200

    added ui fixes and jump/run fixes

--- Assets/Scripts/Casting/Spells/Passive/Jump.cs
+++ Assets/Scripts/Casting/Spells/Passive/Jump.cs
@@ -7,13 +7,11 @@ namespace Assets.Scripts.Casting.Spells
     public class Jump : MonoBehaviour
     {
         [SerializeField] float jumpForce = 1f;
-        float maxJumpForce;
         [SerializeField] float buffTime = 5f;
         void Awake()
         {
             SpellBase sB = GetComponent<SpellBase>();
             sB.spellStartEvent.AddListener(StartSpell);
-            maxJumpForce = PlayerMovement.Instance.jumpForce;
         }
 
         public void StartSpell()
@@ -30,10 +28,10 @@ namespace Assets.Scripts.Casting.Spells
             if(buffTime >= 0)
             {
                 buffTime -= Time.deltaTime;
-                PlayerMovement.Instance.jumpForce = maxJumpForce + jumpForce;
+                PlayerMovement.Instance.jumpForce +=  jumpForce;
                 if(buffTime <= 0)
                 {
-                    PlayerMovement.Instance.jumpForce = maxJumpForce;
+                    PlayerMovement.Instance.jumpForce -= jumpForce;
                 }
             }
         }
--- Assets/Scripts/Casting/Spells/Passive/Run.cs
+++ Assets/Scripts/Casting/Spells/Passive/Run.cs
@@ -7,14 +7,12 @@ namespace Assets.Scripts.Casting.Spells
     public class Run : MonoBehaviour
     {
         [SerializeField] float speedMultiplier = 2f;
-        float maxRunSpeed;
         [SerializeField] float buffTime = 5f;
 
         void Awake()
         {
             SpellBase sB = GetComponent<SpellBase>();
             sB.spellStartEvent.AddListener(StartSpell);
-            maxRunSpeed = PlayerMovement.Instance.movementSpeed;
         }
 
         public void StartSpell()
@@ -27,10 +25,10 @@ namespace Assets.Scripts.Casting.Spells
             if (buffTime >= 0)
             {
                 buffTime -= Time.deltaTime;
-                PlayerMovement.Instance.movementSpeed = maxRunSpeed + speedMultiplier;
+                PlayerMovement.Instance.movementSpeed +=  speedMultiplier;
                 if (buffTime <= 0)
                 {
-                    PlayerMovement.Instance.movementSpeed = maxRunSpeed;
+                    PlayerMovement.Instance.movementSpeed -= speedMultiplier;
                 }
             }
         }
